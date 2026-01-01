b155ac72b0529f847a2e842e21449f4204c8e39b
ren0714poo <ren0714poo@gmail.com>
2026-01-01 20:15:24 +0900

    add

--- app/component/mainvisual/mainvisual.tsx
+++ app/component/mainvisual/mainvisual.tsx
@@ -1,14 +1,26 @@
-import Link from "next/link";
+import Image from "next/image";
+
 export default function Mainvisual() {
-
-//js書く場所
-
-  return(
-    <>
-    {/* <Link href="about" >about</Link> */}
-    <div>
-
-    </div>
-    </>
+  return (
+    <section className="w-full bg-[#f4f1e3] py-20 flex items-center justify-center">
+      <div className="mx-auto flex max-w-8xl items-center gap-16 px-10">
+        
+        <div className="relative">
+          <Image
+            src="/assets/mainview.png"
+            alt="沖縄の海"
+            width={1100}
+            height={700}
+            className="rounded-[40px] object-cover"
+            priority
+          />
+          <div className="absolute -bottom-8 -left-8 h-24 w-24 rounded-full bg-sky-300"></div>
+        </div>
+        <h1 className="text-6xl font-bold leading-tight tracking-wide writing-vertical">
+          沖縄シーサー<br/>
+          制作大作戦！
+        </h1>
+      </div>
+    </section>
   );
 }
