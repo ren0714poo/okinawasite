76b4662b1b577a2daa6252af31dfe6eb71d4fdad
ren0714poo <ren0714poo@gmail.com>
2026-01-08 14:07:42 +0900

    add-list

--- component/list/list.tsx
+++ component/list/list.tsx
@@ -2,28 +2,49 @@ import ShopCard from "@/component/shopCard";
 
 const shops = [
   {
-    image: "/assets/shop1.png",
+    image: "/assets/shop.png",
     title: "work studio kirara",
-    address: "沖縄県那覇市〇〇",
+    address: "沖縄県国頭郡恩納村恩納2767 万座毛活性化施設",
     time: "30分",
   },
   {
-    image: "/assets/shop2.png",
+    image: "/assets/shop.png",
     title: "TRIP CAFE NAHA",
-    address: "沖縄県那覇市△△",
+    address: "沖縄県那覇市牧志2-6-34 たからビル2F",
     time: "1時間",
   },
   {
-    image: "/assets/shop3.png",
+    image: "/assets/shop.png",
     title: "まいまいシーサー",
-    address: "沖縄県〇〇",
+    address: "沖縄県国頭郡本部町字山川969番地 誠もち店美ら海工房2階",
     time: "2〜3時間",
+  },
+  {
+    image: "/assets/shop.png",
+    title: "美ら風(ちゅらかじ)",
+    address: "沖縄県那覇市牧志3-2-50",
+    time: "90分",
+  },
+  {
+    image: "/assets/shop.png",
+    title: "てぃあんだー",
+    address: "沖縄県那覇市松尾1-9-8 103",
+    time: "1時間",
+  },
+  {
+    image: "/assets/shop.png",
+    title: "ドぅーシーラボ",
+    address: "沖縄県宮古島市平良字久貝681-1",
+    time: "50分",
   },
 ];
 export default function Explanation() {
   return (
-    <section className="w-full py-12 flex items-center justify-center text-[#555555]">
-      <div className="flex flex-col items-center justify-center gap-4 mt-16 text-center px-6">
+    <section className="w-full py-12 text-[#555555]">
+      <h1 className="text-xl text-center my-12 text-sky-400 md:text-2xl font-bold">
+        イベント一覧
+      </h1>
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-6 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
         {shops.map((shop, index) => (
           <ShopCard
             key={index}
--- component/shopCard.tsx
+++ component/shopCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function ShopCard({ image, title, address, time }: Props) {
   return (
-    <div className="w-72 rounded-2xl bg-white shadow-lg overflow-hidden">
+    <div className="w-64 rounded-2xl bg-white shadow-lg overflow-hidden transition duration-300 hover:translate-y-3 hover:shadow-2xl">
       <Image
         src={image}
         alt={title}
@@ -17,7 +17,7 @@ export default function ShopCard({ image, title, address, time }: Props) {
         height={180}
         className="h-44 w-full object-cover"
       />
-      <div className="p-4 text-sm text-[#555]">
+      <div className="p-4 text-sm text-[#555] text-left">
         <h3 className="font-bold mb-2">{title}</h3>
         <p className="mb-1">{address}</p>
         <p className="text-xs">所要時間 {time}</p>
